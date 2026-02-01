e9f93d139a2cc4c77a5cb6c7dd4fb70378319cdc
dragan381 <148227209+dragan381@users.noreply.github.com>
2026-02-01 15:38:48 +0100

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://dragan381.github.io',
-  base: '/tehnicki_pregled'
+  site: 'https://dragan381.github.io'
 });
