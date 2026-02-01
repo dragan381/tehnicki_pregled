12ef5847fa626f07968ed1f97f4ff2bd380016b2
dragan381 <148227209+dragan381@users.noreply.github.com>
2026-02-01 18:27:56 +0100

    trailing slash ignore removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,4 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://dragan381.github.io",
   base: process.env.GITHUB_ACTIONS ? "/tehnicki_pregled" : "/",
-  trailingSlash: "ignore",
 });
